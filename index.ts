4a0ae576a5518df16700322f1d518ec4e5c40e44
Alex Vipond <hello@alexvipond.dev>
2024-04-04 10:51:55 -0500

    change variant name to `effect`, update documentation for clarity and readability

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ const signals = plugin(({ matchUtilities, matchVariant, theme }) => {
   );
 
   matchVariant(
-    'signal',
+    'effect',
     (_, { modifier }) => {
       return `@container style(${getStyleVarName(modifier)}: true)`;
     },
