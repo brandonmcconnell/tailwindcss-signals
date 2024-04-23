9e689982f4135db59f1961eae68534c115ad98f6
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-04-23 00:44:25 -0700

    Improve exports

---
+++ dist/index.modern.js
@@ -0,0 +1,2 @@
+import t from"tailwindcss/plugin";function i(){return i=Object.assign?Object.assign.bind():function(t){for(var i=1;i<arguments.length;i++){var n=arguments[i];for(var r in n)Object.prototype.hasOwnProperty.call(n,r)&&(t[r]=n[r])}return t},i.apply(this,arguments)}const n=t=>"--tw-signal"+(t?`_${t}`:""),r={values:{DEFAULT:""}},a=t(({matchUtilities:t,matchVariant:a})=>{t({signal:(t,{modifier:i})=>({[n(i)]:"true"})},i({},r,{modifiers:"any"})),a("signal",(t,{modifier:i})=>`@container style(${n(i)}: true)`,r)});module.exports=a;
+//# sourceMappingURL=index.modern.js.map
